7b5dd1de7ecd1ecb76b529d1af64d9bbbbdcbdb9
Dimitri Charneux <dimitri15.59@hotmail.fr>
2016-02-08 14:37:24 +0100

    ajout des bases des actstrategy

---
+++ TowerDefense/src/strategy/actStrategy/SimpleActStrategy.java
@@ -0,0 +1,12 @@
+package strategy.actStrategy;
+
+
+/**
+ * Act Strategy that simply action the first Entity that can
+ * 
+ * @author CHARNEUX Dimitri, LEPRETRE Rémy 
+ *
+ */
+public class SimpleActStrategy implements ActStrategy{
+
+}
