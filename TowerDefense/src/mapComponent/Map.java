9d64209e9009db27b623d9f39b1515706575b46d
radgeur <remy2112@hotmail.fr>
2016-03-07 17:23:07 +0100

    getCasesOfEntity

--- TowerDefense/src/mapComponent/Map.java
+++ TowerDefense/src/mapComponent/Map.java
@@ -26,10 +26,31 @@ public class Map {
 		listEntities = new ArrayList<Entity>();
 	}
 	
+	/** return the case where the point (x,y) is
+	 * @param x coordinate of the point
+	 * @param y coordinate of the point
+	 * @return case
+	 */
 	public Case getCaseWithPixel(int x, int y){
 		if(x > map.length * casewidth || x< 0) return null;
 		if(y > map[0].length * caseHeight || y< 0) return null;
 		return map[x/caseHeight][y/casewidth];
+	}
+	
+	/** return all the Cases where the entity is
+	 * @param entity to locate
+	 * @return list of Cases where the entity is
+	 */
+	public List<Case> getCasesOfEntity(Entity entity) {
+		List<Case> cases = new ArrayList<Case>();
+		Case start = getCaseWithPixel(entity.getX(), entity.getY());
+		Case end = getCaseWithPixel(entity.getX() + entity.getWidth(), entity.getY() + entity.getHeight());
+		for(int i = start.getX();i<end.getX();i++){
+			for(int j = start.getY();j<end.getY();j++){
+				cases.add(map[i][j]);
+			}
+		}
+		return cases;
 	}
 	
 	public List<Entity> getEntities() {return listEntities;}
--- TowerDefense/src/strategy/moveStrategy/SimpleMoveStrategy.java
+++ TowerDefense/src/strategy/moveStrategy/SimpleMoveStrategy.java
@@ -1,5 +1,7 @@
 package strategy.moveStrategy;
 
+import java.util.List;
+
 import mapComponent.Case;
 import mapComponent.Map;
 import model.GameModel;
@@ -27,13 +29,15 @@ public class SimpleMoveStrategy implements MoveStrategy{
 	public void action(Character character) {
 		Map map = GameModel.map;
 		this.character = character;
-		Case c = map.getCaseWithPixel(character.getX(), character.getY());
-		c.removeCharacter(character);
+		List<Case> cases = map.getCasesOfEntity(character);
+		for(Case caseC : cases)
+			caseC.removeCharacter(character);
 		character.setY(character.getY() + DEFAULT_MOVE_SPEED * character.getSpeed());
 		if (character.getY() + character.getHeight() > map.getMap().length * Map.casewidth)
 			character.setY(map.getMap()[0].length * Map.casewidth - character.getHeight());
-		c = map.getCaseWithPixel(character.getX(), character.getY());
-		c.addCharacter(character);
+		cases = map.getCasesOfEntity(character);
+		for(Case caseC : cases)
+			caseC.addCharacter(character);
 		System.out.println("move" + character.getY());
 	} 
 }
