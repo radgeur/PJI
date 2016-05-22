423c4bafe44d6c29c9854a3d44956346a5144825
radgeur <remy2112@hotmail.fr>
2016-05-23 00:54:16 +0200

    can launch in the twice mode : User and Dev

--- TowerDefense/src/main/CreationMapTest.java
+++ TowerDefense/src/main/CreationMapTest.java
@@ -3,6 +3,7 @@ package main;
 import java.io.IOException;
 
 import model.GameModel;
+import view.GameDevView;
 import view.GameUserView;
 import controler.GameControler;
 
@@ -15,11 +16,22 @@ import controler.GameControler;
 
 public class CreationMapTest {
 	public static void main(String[] args) throws IOException{
-		GameModel model = new GameModel();
-		model.readMap("../map_boucle.txt");
-		GameUserView view = new GameUserView();
-		model.addObserver(view);
-		GameControler controler = new GameControler(model);
-		controler.run();
+		if(args.length == 1 && args[0].equals("root")){
+			GameModel model = new GameModel();
+			model.readMap("../map_boucle.txt");
+			GameDevView view = new GameDevView();
+			model.addObserver(view);
+			GameControler controler = new GameControler(model);
+			controler.run();
+		}
+		else{
+			GameModel model = new GameModel();
+			model.readMap("../map_boucle.txt");
+			GameUserView view = new GameUserView();
+			model.addObserver(view);
+			GameControler controler = new GameControler(model);
+			controler.run();
+		}
+		
 	}
 }
